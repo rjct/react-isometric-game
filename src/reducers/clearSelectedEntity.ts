2148693c28b7793f82146587c30beac2b61e0b60
Eugene Shordukalo <reject@newtonideas.com>
2023-05-02 21:12:08 +0300

    Suppress eslint warn

--- src/reducers/clearSelectedEntity.ts
+++ src/reducers/clearSelectedEntity.ts
@@ -5,6 +5,7 @@ export interface ClearSelectedEntityReducerAction {
   type: "clearSelectedEntity";
 }
 
+// eslint-disable-next-line  @typescript-eslint/no-unused-vars
 export function clearSelectedEntity(state: GameMap, action: ClearSelectedEntityReducerAction): GameMap {
   return { ...state, ...{ selectedEntity: null as unknown as Building } };
 }
