80f3446465b856dc53c43c1e10d1eb22d32f0cd8
Eugene Shordukalo <info@reject.in.ua>
2023-06-23 16:45:20 +0300

    Changed pathfinding method, decreased runnning speed of vault13_male

--- src/engine/pathFinder.ts
+++ src/engine/pathFinder.ts
@@ -1,14 +1,12 @@
 import pathfinding from "pathfinding";
 import { GameMap } from "./GameMap";
-import { floor } from "./helpers";
 
 export function pathFinder(matrix: GameMap["matrix"], fromPos: GridCoordinates, toPos: GridCoordinates) {
-  const grid = new pathfinding.Grid([...matrix]);
-  const finder = new pathfinding.BestFirstFinder({
+  const grid = new pathfinding.Grid(matrix);
+  const finder = new pathfinding.AStarFinder({
     diagonalMovement: pathfinding.DiagonalMovement.Never,
-    heuristic: pathfinding.Heuristic.euclidean,
-    weight: 1,
+    heuristic: pathfinding.Heuristic.chebyshev,
   });
 
-  return finder.findPath(floor(fromPos.x), floor(fromPos.y), floor(toPos.x), floor(toPos.y), grid);
+  return finder.findPath(Math.round(fromPos.x), Math.round(fromPos.y), Math.round(toPos.x), Math.round(toPos.y), grid);
 }
