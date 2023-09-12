b3b1cf337ec7c340dee107f5c836f1227f078152
Eugene Shordukalo <info@reject.in.ua>
2023-09-12 16:37:47 +0300

    refactoring

--- src/components/map/terrain/TerrainAreas.tsx
+++ src/components/map/terrain/TerrainAreas.tsx
@@ -1,100 +1,21 @@
 import { IsometricCanvasMapLayer } from "@src/components/map/IsometricCanvasMapLayer";
-import { constants } from "@src/constants";
-import { gridToScreenSpace } from "@src/engine/helpers";
 import { useGameState } from "@src/hooks/useGameState";
 import { useScene } from "@src/hooks/useScene";
+import { useTerrainAreas } from "@src/hooks/useTerrainAreas";
 import React from "react";
 
 export const TerrainAreas = React.memo(function TerrainAreas() {
   const { gameState, uiState } = useGameState();
   const { checkCurrentScene } = useScene();
+  const { renderTerrainTiles } = useTerrainAreas(gameState, uiState);
 
   const canvasRef = React.createRef<HTMLCanvasElement>();
-  const [ctx, setCtx] = React.useState(null as unknown as CanvasRenderingContext2D);
-
-  const tileWidth = constants.tileSize.width;
-  const tileHeight = constants.tileSize.height;
-
-  const mapWidth = gameState.mapSize.width;
-  const mapHeight = gameState.mapSize.height;
-
-  const renderTerrainTiles = () => {
-    ctx.clearRect(0, 0, mapWidth * tileWidth, mapHeight * tileHeight);
-    ctx.setTransform(1, 0, 0, 1, -uiState.scroll.x, -uiState.scroll.y);
-
-    gameState.terrain.forEach((terrainArea) => {
-      for (let x = terrainArea.target.x1; x < terrainArea.target.x2; x++) {
-        for (let y = terrainArea.target.y1; y < terrainArea.target.y2; y++) {
-          const position = { x, y };
-
-          if (
-            !gameState.isEntityInViewport(
-              {
-                position,
-                size: {
-                  grid: { width: 1, height: 1, length: 1 },
-                  screen: {
-                    width: constants.wireframeTileSize.width,
-                    height: constants.wireframeTileSize.height,
-                  },
-                },
-              },
-              uiState.viewport,
-            )
-          ) {
-            continue;
-          }
-
-          const screenPosition = gridToScreenSpace(position, gameState.mapSize);
-          const terrainTile = terrainArea.tiles.get(`${x}:${y}`);
-
-          if (terrainTile) {
-            const image = gameState.getAssetImage(terrainTile.type)?.source;
-
-            if (image) {
-              ctx.drawImage(
-                image,
-                tileWidth * (terrainTile.x || 0) + (terrainTile.x || 0) * 3 + 1,
-                tileHeight * (terrainTile.y || 0) + (terrainTile.y || 0) * 3 + 1,
-                tileWidth,
-                tileHeight,
-                screenPosition.x,
-                screenPosition.y,
-                tileWidth,
-                tileHeight,
-              );
-            }
-          }
-        }
-      }
-
-      if (terrainArea.exitUrl) {
-        const { x1, y1, x2, y2 } = terrainArea.target;
-
-        const width = x2 - x1;
-        const height = y2 - y1;
-
-        const tl = gridToScreenSpace({ x: x1, y: y1 }, gameState.mapSize);
-        const tr = gridToScreenSpace({ x: x1 + width, y: y1 }, gameState.mapSize);
-        const br = gridToScreenSpace({ x: x2, y: y2 }, gameState.mapSize);
-        const bl = gridToScreenSpace({ x: x1, y: y1 + height }, gameState.mapSize);
-
-        ctx.beginPath();
-        ctx.moveTo(tl.x + tileWidth / 2, tl.y);
-        ctx.lineTo(tr.x + tileWidth / 2, tr.y);
-        ctx.lineTo(br.x + tileWidth / 2, br.y);
-        ctx.lineTo(bl.x + tileWidth / 2, bl.y);
-
-        ctx.fillStyle = "rgba(0,255,0, .2)";
-        ctx.fill();
-      }
-    });
-  };
+  const [ctx, setCtx] = React.useState<CanvasRenderingContext2D | null>(null);
 
   React.useEffect(() => {
     if (!ctx) return;
 
-    renderTerrainTiles();
+    renderTerrainTiles(ctx);
   }, [
     gameState.mapSize,
     gameState.mapUrl,
@@ -104,7 +25,7 @@ export const TerrainAreas = React.memo(function TerrainAreas() {
   ]);
 
   React.useEffect(() => {
-    setCtx(canvasRef.current?.getContext("2d") as CanvasRenderingContext2D);
+    setCtx(canvasRef.current ? canvasRef.current.getContext("2d") : null);
   }, []);
 
   return <IsometricCanvasMapLayer className={"terrain"} ref={canvasRef} />;
