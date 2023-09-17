9f131e740409ed5c7ade9627952ae9697f0f00fb
Eugene Shordukalo <info@reject.in.ua>
2023-09-17 17:15:21 +0300

    proper centering hero

--- src/reducers/ui/centerMapOnHero.ts
+++ src/reducers/ui/centerMapOnHero.ts
@@ -1,3 +1,4 @@
+import { constants } from "@src/constants";
 import { GameUI } from "@src/context/GameUIContext";
 
 export type CenterMapOnHeroUIReducerAction = {
@@ -9,7 +10,7 @@ export function centerMapOnHero(state: GameUI, action: CenterMapOnHeroUIReducerA
   const { x1, y1, x2, y2 } = state.viewport.screen;
 
   state.setScroll({
-    x: action.unitCoordinates.x - (x2 - x1) / 2,
+    x: action.unitCoordinates.x - (x2 - x1) / 2 + constants.tileSize.width / 2,
     y: action.unitCoordinates.y - (y2 - y1) / 2,
   });
 
